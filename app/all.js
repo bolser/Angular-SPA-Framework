5ff6d5c237e2acb1cc3134ca3584b2661ae676a8
Rich Cooper <rich@steamhousedesigns.com>
2016-04-24 16:38:53 +0100

    Refactored screen variables and SASS grid to use maps and be completely DRY

--- app/all.js
+++ app/all.js
@@ -2,23 +2,23 @@
 // --------------------------------
 
 //config
-require('../config.js');
+require('config.js');
 
 //services
-require('../services/example.service.js');
+require('services/example.service.js');
 
 //run
-require('../run/view.run.js');
-require('../run/analytics.run.js');
+require('run/view.run.js');
+require('run/analytics.run.js');
 
 //routes
-require('../routes/routes.js');
+require('routes/routes.js');
 
 //directives
-require('../directives/email-mask.directive.js');
-require('../directives/placeholder-support.directive.js');
-require('../directives/example.directive.js');
+require('directives/email-mask.directive.js');
+require('directives/placeholder-support.directive.js');
+require('directives/example.directive.js');
 
 //controllers
-require('../controllers/contact.controller.js');
-require('../controllers/example.controller.js');
+require('controllers/contact.controller.js');
+require('controllers/example.controller.js');
