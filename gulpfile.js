f94aac07ca85bcf9785578faf7141dcea9290402
Rich Cooper <rcooper@bolser.co.uk>
2016-09-27 17:29:13 +0100

    Reformatted environment tasks to work without APP_ENV

--- gulpfile.js
+++ gulpfile.js
@@ -9,15 +9,21 @@ var env = (process.env.APP_ENV ? process.env.APP_ENV : 'development');
 
 // Tasks
 gulp.task('clean', require('./tasks/clean'));
-gulp.task('css', require('./tasks/css')[env]);
 gulp.task('fonts', require('./tasks/fonts'));
 gulp.task('html', require('./tasks/html'));
-gulp.task('images', require('./tasks/images')[env]);
 gulp.task('index', require('./tasks/index'));
-gulp.task('js', require('./tasks/js')[env]);
-gulp.task('js-libs', require('./tasks/js-libs')[env]);
 gulp.task('rev', require('./tasks/rev'));
 gulp.task('watch', require('./tasks/watch'));
+
+// Environment tasks
+envTasks('development');
+
+function envTasks(env) {
+  gulp.task('css', require('./tasks/css')[env]);
+  gulp.task('images', require('./tasks/images')[env]);
+  gulp.task('js', require('./tasks/js')[env]);
+  gulp.task('js-libs', require('./tasks/js-libs')[env]);
+}
 
 // Development build
 gulp.task('default', function() {
@@ -31,6 +37,7 @@ gulp.task('default', function() {
 
 // Production build
 gulp.task('production', function() {
+  envTasks('production');
   runSequence(
     'clean',
     'html',
